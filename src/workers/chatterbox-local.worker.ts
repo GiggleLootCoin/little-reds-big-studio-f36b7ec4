e84fcffdbade0e2e7da6f453f05fead4461c4301
LittleRedBigSmile <gigglelootcoin@gmail.com>
2026-08-15 16:09:46 +0100

    Fix browser worker audio transfer typing

--- src/workers/chatterbox-local.worker.ts
+++ src/workers/chatterbox-local.worker.ts
@@ -88,7 +88,7 @@ async function generate(text: string, exaggeration: number) {
     temperature: 0.2,
   });
   const data = waveform.data;
-  const buffer = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength);
+  const buffer = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength) as ArrayBuffer;
   self.postMessage({ type: "audio", sampleRate: 24000, waveform: buffer }, [buffer]);
 }
 
